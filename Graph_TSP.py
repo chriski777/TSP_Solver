d0fc8c42ebfb5883aa0da05934fd1ab0945bb473
Chris Ki <chriski777@berkeley.edu>
2017-04-22 23:09:50 -0700

    Created Graph_TSP class

---
+++ Graph_TSP.py
@@ -0,0 +1,8 @@
+class Graph_TSP:
+	#Nodes should be a dictionary of key value pairing : node num to xy coordinates
+	#Edges are implied in the adjacency matrix 
+	#Adjacency matrix will be n x n; where n is the number of nodes
+	def __init__(self, nodes, adjacency_matrix):
+		self.nodes = nodes
+		self.adjacency_matrix = adjacency_matrix
+
