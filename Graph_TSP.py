45ce0be07a1e30e814e4b18f882bcd7fa3e7c21e
Chris Ki <chriski777@berkeley.edu>
2017-04-24 05:05:05 -0700

    Finished Greedy Algorithm, looks great time to work on visualizer now. Also, I added a disjoint_sets class which helps check for cycles.

--- Graph_TSP.py
+++ Graph_TSP.py
@@ -1,5 +1,7 @@
 import random
+import operator
 import numpy as np
+import disjoint_sets as DS
 class Graph_TSP:
 	#Nodes should be a dictionary of key value pairing : node num to xy coordinates
 	#Edges are implied in the adjacency matrix 
@@ -8,10 +10,28 @@ class Graph_TSP:
 		self.nodeDict = nodeDict
 		self.adjMatrix = adjMatrix
 		self.counts = len(nodeDict)
+		self.edgeDict = {}
+		for i in range(self.counts):
+		    for j in range(i+1, self.counts):
+		    	vertices = (i,j)
+		    	self.edgeDict[vertices] = self.adjMatrix[i,j]
 	def randomSolution(self):
 		unvisitedNodes = range(0,self.counts)
 		random.shuffle(unvisitedNodes)
 		return unvisitedNodes
+	########
+	#### NearestNeighbor
+	#### 	Initialize visitedNodes to ensure no cycle is created 
+	####	unvisitedNodes: list of unvisited nodes
+	####	1. Pick a random node, then proceed to get that node's neighbors.
+	####	2. Within that node's neighbors, select the minimum edge weight. 
+	####	3. Based on the minimum edge weight, find the index of that weight in the original matrix.
+	####  	4. If that index is NOT in the visitedNodes, remove it from the unvistedNodes list and add it 
+	####	   to the visitedNodes.
+	####	5. Else, remove the minIndex from the edges array and start over from step 2.
+	####	6. Once you remove all the elements of unvisitedNode, terminate and return the sequence of 
+	####	   vertices you will follow. 
+	########
 	def nearestNeighbor(self):
 		visitedNodes = []
 		unvisitedNodes = range(0,self.counts)
@@ -32,14 +52,53 @@ class Graph_TSP:
 		        else:
 		            minEdgeIndex = np.where(edges == minVal)[0][0]
 		            edges = np.delete(edges,minEdgeIndex)
-		return visitedNodes
-	#Consider same edge lengths
+		edgePath = []
+		for i in range(0,self.counts):
+			if i < self.counts - 1:
+				edgePath.append((visitedNodes[i],visitedNodes[i+1]))
+			else:
+				edgePath.append((visitedNodes[i],visitedNodes[0]))
+		return edgePath
+	########
+	#### Greedy Search
+	#### 	1. Sort the edges by weight values.
+	####	2. Select the least-valued edge.
+	####	3. Make sure it does not form a cycle if added. I do this by checking if both vertices are in
+	####	   visitedNodes. This is accomplished with a small helper function isCycle. 
+	####	4. Check also if the two nodes have less than degree 2. 
+	####    5. If both constraints apply, add 1 to each degree and also change visitedNodes. Make sure to 
+	####	   remove the edge that we added. 
+	####    6. Start from the next least-value again and check each to make sure no cycle is formed and all degrees
+	####       are less than 2. 
+	########
 	def greedy(self):
-		uniqueEdges = []
-		for i in range(self.counts):
-		    for j in range(i+1, self.counts):
-		        uniqueEdges.append(self.adjMatrix[i,j])		
-		return 1
+		allNodes = []
+		edgePath = []
+		for node in range(0,self.counts):
+			allNodes.append(DS.disjoint_set(node))
+		sorted_edges = sorted(self.edgeDict.items(), key=operator.itemgetter(1))
+		degreeDict = {element: 0 for element in allNodes}
+		numEdges = 0
+		startNode = allNodes[sorted_edges[0][0][0]]
+		while numEdges < self.counts - 1:
+			for edge in sorted_edges:
+				vertices = edge[0]
+				ds1 = allNodes[vertices[0]]
+				ds2 = allNodes[vertices[1]]
+				if not(self.isCycle(ds1,ds2)) and self.nodeLessTwo(ds1,ds2,degreeDict):
+					ds1.joinSets(ds2)
+					degreeDict[ds1] += 1
+					degreeDict[ds2] += 1
+					numEdges += 1
+					edgePath.append(vertices)
+		lastTwo = [allNodes.index(x) for x in degreeDict.keys() if degreeDict[x] == 1]
+		print degreeDict.values()
+		edgePath.append((lastTwo[0],lastTwo[1]))
+ 		return edgePath
+ 	def isCycle(self,ds1,ds2):
+ 		return ds1.find() == ds2.find()
+	def nodeLessTwo(self, d1, d2, degreeDict):
+		return (degreeDict[d1] < 2) and (degreeDict[d2] < 2)
 	def convexhullInsert(self):
 		return 1
 	def HKLowerBoundCost(self):
