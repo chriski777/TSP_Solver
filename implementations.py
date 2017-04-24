278d0a854d2b2d0d0dbabeb594f300ae9c885208
Chris Ki <chriski777@berkeley.edu>
2017-04-23 19:26:44 -0700

    added the implementations file which will be the final execution file

--- implementations.py
+++ implementations.py
@@ -1,3 +1,26 @@
 import plotly 
+import plotly.plotly as py
+from plotly.graph_objs import *
+import networkx as nx
+import Graph_TSP as G
+import dataset_processing as D
+import numpy as np
 plotly.tools.set_credentials_file(username='chriski777', api_key='PE1PC0QMswvn4XZSZMHg')
+
+#Data directory names
+dataDir = "datasetTSP"
+att48 = "att48"
+
+######################
+###Data Processing
+######################
+#Feed in a directory which has your xy coordinates and your adjacency matrices
+att48_DS = D.dataset_processing(dataDir,att48)
+
+######################
+###Initialization
+######################
+att48_graph = G.Graph_TSP(att48_DS.nodeDict,att48_DS.adjMatrix)
+
+
 
